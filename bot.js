ad645b2a43ec6519275e1cf89a315fe79bcef021
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-04 11:11:56 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 if(!Discord) var Discord = require('discord.js');
 if(!client) var client = new Discord.Client();
-if(!prefix) var prefix = "r-";
+if(!prefix) var prefix = "!-";
 
 const adminprefix = "r-";
 const devs = ['504250844934438922'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
-if (message.content.startsWith(adminprefix + 'st')) {
+if (message.content.startsWith(adminprefix + 'mhs')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk")
     message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(5000)})
 }
